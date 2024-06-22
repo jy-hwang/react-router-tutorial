4149206fc312037a6cdfb3546918b6f95f576b03
jy-hwang <stratojy@gmail.com>
2024-06-22 15:25:50 +0900

    ContextAPI 를 사용한 전역 값 관리
    1. createContext 로 기본값 적용
    2. 넘겨 받을 Child 에서 useContext 이용
    3. Provider 객체로 감싸줌.
    4. 버튼을 하나 만들어서 onClick 이벤트 적용해보기.

--- src/components/ContextSample.js
+++ src/components/ContextSample.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
-function Child({ text }) {
+const MyContext = createContext('defaultValue');
+
+function Child() {
+  const text = useContext(MyContext);
   return <div>안녕하세요? {text}</div>;
 }
 
-function Parent({ text }) {
-  return <Child text={text} />;
+function Parent() {
+  return <Child />;
 }
 
-function GrandParent({ text }) {
-  return <Parent text={text} />;
+function GrandParent() {
+  return <Parent />;
 }
 
 function ContextSample() {
-  return <GrandParent text="GOOD" />;
+  const [value, setValue] = useState(true);
+  return (
+    <MyContext.Provider value={value ? 'Great' : 'Bad'}>
+      <GrandParent />
+      <button onClick={() => setValue(!value)}>CLICK ME!</button>
+    </MyContext.Provider>
+  );
 }
 
 export default ContextSample;
