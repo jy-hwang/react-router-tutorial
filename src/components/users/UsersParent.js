61bb8024ac1878a4253dbe9a2de138f82da770d8
jy-hwang <stratojy@gmail.com>
2024-06-22 14:39:24 +0900

    UserParent 컴포넌트 useReducer로 변경해보기
    1. useState 쓰고 있던 것을 useReducer로 변경해보기
    2. 초기값 주는 부분을 initialState 로 만들기
    3. reducer 함수 만들기.
    4. onChange -> CHANGE_INPUT 형태로 하나씩 변경하기.

--- src/components/users/UsersParent.js
+++ src/components/users/UsersParent.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo, useCallback } from 'react';
+import React, { useRef, useReducer, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -8,57 +8,47 @@ function countActiveUsers(users) {
 }
 
 function UsersParent() {
-  console.log('rerendering..');
-  const [inputs, setInputs] = useState({
-    username: '',
-    email: '',
-  });
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const { username, email } = inputs;
+  const { users } = state;
 
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setInputs({
-        ...inputs,
-        [name]: value,
-      });
-    },
-    [inputs],
-  );
+  const { username, email } = state.inputs;
 
-  const [users, setUsers] = useState([
-    { id: 1, username: 'jackie', email: 'jackie@abc.com', active: true },
-    { id: 2, username: 'michael', email: 'michael@abc.com', active: false },
-    { id: 3, username: 'tester', email: 'tester@abc.com', active: false },
-    { id: 4, username: 'liz', email: 'liz@abc.com', active: true },
-  ]);
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value,
+    });
+  }, []);
+
   const nextId = useRef(5);
 
   const onCreate = useCallback(() => {
-    console.log(nextId.current, username, email);
-
-    const user = {
-      username: username,
-      email: email,
-      id: nextId.current,
-    };
-
-    setUsers((users) => users.concat(user));
-    setInputs({ username: '', email: '' });
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      },
+    });
     nextId.current += 1;
   }, [username, email]);
 
   const onRemove = useCallback((id) => {
-    setUsers((users) => users.filter((user) => user.id !== id));
+    dispatch({
+      type: 'REMOVE_USER',
+      id,
+    });
   }, []);
 
   const onToggle = useCallback((id) => {
-    setUsers((users) =>
-      users.map((user) =>
-        user.id === id ? { ...user, active: !user.active } : user,
-      ),
-    );
+    dispatch({
+      type: 'TOGGLE_USER',
+      id,
+    });
   }, []);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
@@ -78,3 +68,49 @@ function UsersParent() {
 }
 
 export default React.memo(UsersParent);
+
+const initialState = {
+  inputs: {
+    username: '',
+    email: '',
+  },
+  users: [
+    { id: 1, username: 'jackie', email: 'jackie@abc.com', active: true },
+    { id: 2, username: 'michael', email: 'michael@abc.com', active: false },
+    { id: 3, username: 'tester', email: 'tester@abc.com', active: false },
+    { id: 4, username: 'liz', email: 'liz@abc.com', active: true },
+  ],
+};
+
+function reducer(state, action) {
+  console.log(state, action);
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value,
+        },
+      };
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user),
+      };
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === action.id ? { ...user, active: !user.active } : user,
+        ),
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.id),
+      };
+    default:
+      throw new Error('Unhandled action');
+  }
+}
